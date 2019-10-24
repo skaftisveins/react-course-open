6417fbcce17c596280a7f86787cfed37bf9fa8c5
ss1.h18@nemandi.ntv.is <skaftisveins@gmail.com>
2019-10-24 14:39:17 +0000

    injecting mongoDB and API context for expenses

--- Routes/expenses.js
+++ Routes/expenses.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const Expenses = require('../models/Expenses');
+
 // @route   GET api/expenses
 // @desc    Get all expenses
-// @access  Private
+// @access  Public
 router.get('/', (req, res) => {
+  try {
+    // const expenses = await Expenses
+  } catch (err) {}
   res.send('Get all expenses');
 });
 
 // @route   POST api/expenses
 // @desc    Add new expenses
-// @access  Private
+// @access  Public
 router.post('/', (req, res) => {
   res.send('Add Expense');
 });
 
 // @route   PUT api/expenses
 // @desc    Update new expenses
-// @access  Private
+// @access  Public
 router.put('/', (req, res) => {
   res.send('Update Expense');
 });
 
 // @route   DELETE api/expenses
 // @desc    Delete new expenses
-// @access  Private
+// @access  Public
 router.delete('/', (req, res) => {
   res.send('Delete Expense');
 });
